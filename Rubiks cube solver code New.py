d7b13c10f383c291dadb7a296acdb1ba68b25104
TarTarBinks-dev <amazingmemc@gmail.com>
2021-05-26 17:28:16 -0700

    Xturn update

--- Rubiks cube solver code New.py
+++ Rubiks cube solver code New.py
@@ -75,10 +75,10 @@ solvedC7 = False
 solvedC8 = False
 
 def Xturn():
-    GPIO.output(DIR5, CW)
-    GPIO.output(DIR8, CCW)
-    GPIO.output(DIR1, CCW)
-    GPIO.output(DIR3, CCW)
+    GPIO.output(DIR5, CCW)
+    GPIO.output(DIR8, CW)
+    GPIO.output(DIR1, CW)
+    GPIO.output(DIR3, CW)
     for x in range(30):
         GPIO.output(STEP1, GPIO.HIGH)
         GPIO.output(STEP3, GPIO.HIGH)
@@ -94,8 +94,8 @@ def Xturn():
         GPIO.output(STEP8, GPIO.LOW)
         sleep(delay)
         
-    GPIO.output(DIR1, CW)
-    GPIO.output(DIR3, CW)
+    GPIO.output(DIR1, CCW)
+    GPIO.output(DIR3, CCW)
     for x in range(30):
         GPIO.output(STEP1, GPIO.HIGH)
         GPIO.output(STEP3, GPIO.HIGH)
@@ -104,7 +104,7 @@ def Xturn():
         GPIO.output(STEP3, GPIO.LOW)
         sleep(delay)
     
-    GPIO.output(DIR2, CCW)
+    GPIO.output(DIR2, CW)
     GPIO.output(DIR4, CCW)
     for x in range(30):
         GPIO.output(STEP2, GPIO.HIGH)
@@ -114,8 +114,8 @@ def Xturn():
         GPIO.output(STEP4, GPIO.LOW)
         sleep(delay)
 
-    GPIO.output(DIR5, CCW)
-    GPIO.output(DIR8, CW)
+    GPIO.output(DIR5, CW)
+    GPIO.output(DIR8, CCW)
     for x in range(50):
         GPIO.output(STEP5, GPIO.HIGH)
         GPIO.output(STEP8, GPIO.HIGH)
@@ -124,7 +124,7 @@ def Xturn():
         GPIO.output(STEP8, GPIO.LOW)
         sleep(delay)
 
-    GPIO.output(DIR2, CW)
+    GPIO.output(DIR2, CCW)
     GPIO.output(DIR4, CW)
     for x in range(30):
         GPIO.output(STEP2, GPIO.HIGH)
@@ -135,25 +135,8 @@ def Xturn():
         sleep(delay)
 
 def Xprimeturn():
-    GPIO.output(DIR5, CCW)
-    GPIO.output(DIR8, CW)
-    GPIO.output(DIR1, CCW)
-    GPIO.output(DIR3, CCW)
-    for x in range(30):
-        GPIO.output(STEP1, GPIO.HIGH)
-        GPIO.output(STEP3, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP1, GPIO.LOW)
-        GPIO.output(STEP3, GPIO.LOW)
-        sleep(delay)
-    for x in range(50):
-        GPIO.output(STEP5, GPIO.HIGH)
-        GPIO.output(STEP8, GPIO.HIGH)
-        sleep(delay)
-        GPIO.output(STEP5, GPIO.LOW)
-        GPIO.output(STEP8, GPIO.LOW)
-        sleep(delay)
-        
+    GPIO.output(DIR5, CW)
+    GPIO.output(DIR8, CCW)
     GPIO.output(DIR1, CW)
     GPIO.output(DIR3, CW)
     for x in range(30):
@@ -163,8 +146,25 @@ def Xprimeturn():
         GPIO.output(STEP1, GPIO.LOW)
         GPIO.output(STEP3, GPIO.LOW)
         sleep(delay)
+    for x in range(50):
+        GPIO.output(STEP5, GPIO.HIGH)
+        GPIO.output(STEP8, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP5, GPIO.LOW)
+        GPIO.output(STEP8, GPIO.LOW)
+        sleep(delay)
+        
+    GPIO.output(DIR1, CCW)
+    GPIO.output(DIR3, CCW)
+    for x in range(30):
+        GPIO.output(STEP1, GPIO.HIGH)
+        GPIO.output(STEP3, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP1, GPIO.LOW)
+        GPIO.output(STEP3, GPIO.LOW)
+        sleep(delay)
     
-    GPIO.output(DIR2, CCW)
+    GPIO.output(DIR2, CW)
     GPIO.output(DIR4, CCW)
     for x in range(30):
         GPIO.output(STEP2, GPIO.HIGH)
@@ -174,8 +174,8 @@ def Xprimeturn():
         GPIO.output(STEP4, GPIO.LOW)
         sleep(delay)
 
-    GPIO.output(DIR5, CW)
-    GPIO.output(DIR8, CCW)
+    GPIO.output(DIR5, CCW)
+    GPIO.output(DIR8, CW)
     for x in range(50):
         GPIO.output(STEP5, GPIO.HIGH)
         GPIO.output(STEP8, GPIO.HIGH)
@@ -184,7 +184,7 @@ def Xprimeturn():
         GPIO.output(STEP8, GPIO.LOW)
         sleep(delay)
         
-    GPIO.output(DIR2, CW)
+    GPIO.output(DIR2, CCW)
     GPIO.output(DIR4, CW)
     for x in range(30):
         GPIO.output(STEP2, GPIO.HIGH)
@@ -314,7 +314,6 @@ def Yprimeturn():
         GPIO.output(STEP3, GPIO.LOW)
         sleep(delay)
 
-def Rturn():
 
 
 def AlteredYPermutation():
